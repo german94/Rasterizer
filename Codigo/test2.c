7cdd90c050871bda8444ff1f87e1128475c0632a
angelmore <angel_21_fer@hotmail.com>
2016-01-13 23:21:36 -0300

    eso

--- Codigo/test2.c
+++ Codigo/test2.c
@@ -1,12 +1,16 @@
 #include "sdlHelper.h"
 #include "mathHelper.h"
-#include <stdio.h>
+#include "vertexdata.h"
+
+
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
 SDL_Window* window = NULL;
 SDL_Surface* screenSurface = NULL;
+
+int mini(int a, int b){ if (a < b) {return a;} else {return b;} }
 
 bool init()
 {
@@ -26,33 +30,28 @@ bool init()
     return true;
 }
 
-//PASOS:
-//1. Crear la matriz view (v) y projection (p)
-//2. Crear los vertices del modelo 3D
-//3. Crear la matriz de world (w)
-//4. En el loop del render, multiplicar cada vertice del modelo por wvp. Posteriormente dividir por z (o -z) las coordenadas x e y
-//5. Ahora falta mapear las coordenadas x e y de cada punto, a pixeles. Para esto: x = (x + width / 2) / width y lo mismo para y con height.
-//6. El ultimo paso para convertir a pixeles es hacer el redondeo de las coordenadas x e y multiplicadas por width y height. Ademas hay que
-//   invertir la coordenada y: y = floor((1- y) * height), x = floor(x * width) 
 
 int main( int argc, char* args[] )
 {
+
     bool quit = false;
     SDL_Event e;
 
-    Mat4 world, view, proj, wvp;
+    Mat4 world, view, wvp, wv, proj, worldToCamera;
+
+    float angleOfView = 0.78; 
+    float near = 0.1; 
+    float far = 100; 
+
+    //deg to rad mpi/180
+
     Vec3 up = { 0, 1, 0 };
     Vec3 target = { 0, 0, -1 };
-    Vec3 pos = { 0, 0, 3 };
+    Vec3 pos = { 0, 0, 3};
+
     CreateViewMatrix(view, target, pos, up);
-    CreateProjectionMatrix(proj, 0.001f, 1.0f, 0.78f, SCREEN_WIDTH / SCREEN_HEIGHT);
-    Vec3 posCube = { 3, 0, 0 };
-    CreateTranslationMatrix(world, posCube);
-    Mat4Product(world, view, view);
-    Mat4Product(view, proj, wvp);
-    //DisplayMatrix(world);
-    //DisplayMatrix(view);
-    //DisplayMatrix(proj);
+   
+    CreateProjectionMatrix(proj, near, far, angleOfView, 1.0f);
 
     float a = 0;
 
@@ -60,16 +59,41 @@ int main( int argc, char* args[] )
     {
     	screenSurface = SDL_GetWindowSurface( window );
 		
-        float cubo[8][4] = {
-           {-1, 1, 1, 1},
-           {1, 1, 1, 1},
-           {-1, -1, 1, 1},
-           {-1, -1, -1, 1},
-           {-1, 1, -1, 1},
-           {1, 1, -1, 1},
-           {1, -1, 1, 1},
-           {1, -1, -1, 1}
-        };
+    	int numVertices = 8; 
+      float vertices[8][3] = {
+        {-1.0, 1.0,  1.0},
+        {1.0, 1.0,  1.0},
+        {-1.0, -1.0,  1.0},
+       
+       {1.0,  -1.0,  1.0},
+        {-1.0, 1.0, -1.0},
+        {1.0,  1.0, -1.0},
+        {1.0, -1.0, -1.0},
+        {-1.0,  -1.0, -1.0}};
+
+        int lineas[12][3] = {
+        {0,1,2},
+        {1,2,3},
+        {1,3,6},
+        {1,5,6},
+        {0,1,4},
+        {1,4,5},
+        {2,3,7},
+        {3,6,7},
+        {0,2,7},
+        {0,4,7},
+        {4,5,6},
+        {4,6,7}};
+
+        int j;
+
+        Mat4 t;
+	
+		t[0][0] = 1;	t[0][1] = 0;	t[0][2] = 0;	t[0][3] = 0;
+		t[1][0] = 0;	t[1][1] = -1;	t[1][2] = 0;	t[1][3] = 0;
+		t[2][0] = 0;	t[2][1] = 0;	t[2][2] = 1;	t[2][3] = 0;
+		t[3][0] = 0;	t[3][1] = 0;	t[3][2] = 10;	t[3][3] = 1;
+
 
 		while(!quit)
 		{
@@ -79,22 +103,52 @@ int main( int argc, char* args[] )
 					quit = true;
 			}
 
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0x00 ));
+      SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0x00 ) );
 
-            int j;
-            for(j = 0; j < 8; j++)
-            {
-                Vec4 res;
-                Vec4Mat4Product(cubo[j], wvp, res);
-                float x = ((res[0] / (-cubo[j][2])) + SCREEN_WIDTH / 2) / SCREEN_WIDTH; 
-                float y = ((res[1] / (-cubo[j][2])) + SCREEN_HEIGHT / 2) / SCREEN_HEIGHT;
-                
-                putpixel(screenSurface, floor(x * SCREEN_WIDTH), floor((1 - y) * SCREEN_HEIGHT), 0xFF0000);
-            }
+      Mat4 world1;
 
-	        SDL_UpdateWindowSurface( window );
-    	}
+      CreateRotationYMatrix(world1, a);
+
+      Mat4Product(world1, t, world);
+
+      Mat4Product(world,view, wv);
+      Mat4Product(wv, proj, wvp);
+
+      a+=0.001f;
+      	int i, x0, x1, y0, y1;
+		for(j = 0; j < 12; j++)
+    	{
+
+    		for(i = 0; i < 2; i++)
+    		{
+    			Vec4 res;
+	            Vec3Mat4Product(vertices[lineas[j][i]], wvp, res);
+	            x0 = (res[0]/res[3])*SCREEN_WIDTH*0.5 + SCREEN_WIDTH*0.5 ;
+				y0 = (res[1]/res[3])*SCREEN_HEIGHT*0.5 + SCREEN_HEIGHT*0.5; 
+	  		 	
+				//SDL_FillRect( screenSurface, &srcrect, SDL_MapRGB( screenSurface->format, 0xFF, 0x00, 0x00 ) );
+				Vec3Mat4Product(vertices[lineas[j][i+1]], wvp, res);
+				x1 = (res[0]/res[3])*SCREEN_WIDTH*0.5 + SCREEN_WIDTH*0.5 ;
+				y1 = (res[1]/res[3])*SCREEN_HEIGHT*0.5 + SCREEN_HEIGHT*0.5;
+	
+				DrawBline(  x0,  x1,  y0,  y1, screenSurface);
+    		}
+    		Vec4 res;
+
+            Vec3Mat4Product(vertices[lineas[j][2]], wvp, res);
+            x0 = (res[0]/res[3])*SCREEN_WIDTH*0.5 + SCREEN_WIDTH*0.5 ;
+			y0 = (res[1]/res[3])*SCREEN_HEIGHT*0.5 + SCREEN_HEIGHT*0.5; 
+  		 	
+			Vec3Mat4Product(vertices[lineas[j][0]], wvp, res);
+			x1 = (res[0]/res[3])*SCREEN_WIDTH*0.5 + SCREEN_WIDTH*0.5 ;
+			y1 = (res[1]/res[3])*SCREEN_HEIGHT*0.5 + SCREEN_HEIGHT*0.5;
+
+			DrawBline(  x0,  x1,  y0,  y1, screenSurface);
+  		}
+	  	
+        SDL_UpdateWindowSurface( window );
     }
+  }
 
     SDL_DestroyWindow( window );
 
@@ -102,3 +156,5 @@ int main( int argc, char* args[] )
 
     return 0;
 }
+
+
